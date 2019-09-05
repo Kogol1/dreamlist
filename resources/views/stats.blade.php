10c302b6422ac05debf7ffed18725ea58d1ab8ae
„Matyas <matyas.moudry@websta.de>
2019-09-05 23:01:42 +0200

    index > upgrade, stats > basic stats

--- resources/views/stats.blade.php
+++ resources/views/stats.blade.php
@@ -14,15 +14,48 @@
                 <td>{{$not_done}}</td>
             </tr>
             <tr>
+                <th>Celkem zadaných snů</th>
+                <td>{{$done + $not_done}}</td>
+            </tr>
+            <tr>
+                <th>Smazaných snů</th>
+                <td>#</td>
+            </tr>
+            <tr>
                 <th>Poměr splněných/nesplněných</th>
                 <td>{{$kd_ratio}}</td>
             </tr>
         </table>
+        <div id="kd_ratio" style="width: 100%; min-height:450px;"></div>
+
     </div>
 
     <div class="container mt-4">
         <h2>Počet splněných snů v měsících</h2>
     </div>
 
+
+    <script type="text/javascript">
+        google.charts.load("current", {packages:["corechart"]});
+        google.charts.setOnLoadCallback(drawChart);
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+                ['Task', 'Hours per Day'],
+                ['Splněno', {{$done}}],
+                ['Nesplněno', {{$not_done}}]
+            ]);
+
+            var options = {
+                title: 'Poměr splněných/nesplněných snů',
+                pieHole: 0.6,
+            };
+
+            var chart = new google.visualization.PieChart(document.getElementById('kd_ratio'));
+            chart.draw(data, options);
+        }
+        $(window).resize(function(){
+            drawChart();
+        });
+    </script>
 
 @endsection
